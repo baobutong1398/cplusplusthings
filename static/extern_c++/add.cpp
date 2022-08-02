1ec93eba21b6b2dcdcea0e30d3d7084552fc2ff4
baobutong <jackcap2011@gmail.com>
2022-08-03 00:35:29 +0800

    first commit
    Signed-off-by: baobutong <jackcap2011@gmail.com>

---
+++ static/extern_c++/add.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#include "add.h"
+
+int main () {
+    add (2, 3);
+    cout << " run ok" << endl;
+    return 0;
+}
---
+++ static/extern_c++/add.h
@@ -0,0 +1,12 @@
+#ifndef _ADD_H
+#define _ADD_H
+
+#ifdef __cplusplus 
+extern "C"{
+#endif
+int add(int x, int y);
+#ifdef __cplusplus 
+}
+#endif
+
+#endif //_ADD_H
